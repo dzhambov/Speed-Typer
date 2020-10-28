9b1d9489a25fb0481627340388896bc089ae2bb2
Hristo <owner@iMac.local>
2020-10-28 13:37:43 -0700

    add a timer.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings-btn');
 const settingsForm = document.getElementById('settings-form');
@@ -19,6 +19,12 @@ let score = 0;
 
 // Init time 
 let time = 10;
+
+// Focus on text start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
 
 // Generate random word from array
 function getRandomWord() {
@@ -37,18 +43,46 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+function updateTime() {
+  time --;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    // End game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 // Event listeners
 text.addEventListener('input', e => {
   const insertedText = e.target.value;
+
   if (insertedText === randomWord) {
     addWordToDOM();
 
     // Update score
     updateScore();
 
-    // Cleare
+    // Clear
     e.target.value = '';
+
+    time += 5;
+
+    updateTime();
   }
 });
